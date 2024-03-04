ccfa34f4a24da0ede72db3ad2cece958a5046c75
Pablo Quiroga <pabloquiroga@outlook.com.br>
2024-03-04 19:15:33 -0300

    alteração template string

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -60,7 +60,7 @@ function reiniciarJogo() {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e ' + numeroLimite);
     document.getElementById('reiniciar').setAttribute('disabled', true);
 }
 
